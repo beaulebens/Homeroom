347c32c3351d2dbc95af641791f4f87434659432
Beau Lebens <beau@dentedreality.com.au>
2013-09-22 12:11:59 -0400

    Make sidebar optional
    If you remove all widgets, the sidebar will now completely disappear.
    Props @evansolomon for the idea.

--- sidebar.php
+++ sidebar.php
@@ -12,27 +12,11 @@
 				<?php do_action( 'before_sidebar' ); ?>
 
 				<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-					<aside id="search" class="widget widget_search">
-						<?php get_search_form(); ?>
-					</aside>
-
-					<aside id="archives" class="widget">
-						<h1 class="widget-title"><?php _e( 'Archives', 'homeroom' ); ?></h1>
-						<ul>
-							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-						</ul>
-					</aside>
-
-					<aside id="meta" class="widget">
-						<h1 class="widget-title"><?php _e( 'Meta', 'homeroom' ); ?></h1>
-						<ul>
-							<?php wp_register(); ?>
-							<li><?php wp_loginout(); ?></li>
-							<?php wp_meta(); ?>
-						</ul>
-					</aside>
-
+					<style>
+					#content {
+						margin: 0;
+					}
+					</style>
 				<?php endif; // end sidebar widget area ?>
 
 				<?php do_action( 'after_sidebar' ); ?>
